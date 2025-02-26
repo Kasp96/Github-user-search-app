288ab0d67c1dcba151b89f09e480105479445bde
Kacper <kacperwach96@gmail.com>
2025-02-26 19:47:32 +0100

    Add change theme animation feature

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,9 +2,9 @@ const lightBtn = document.querySelector('.light-btn');
 const darkBtn = document.querySelector('.dark-btn');
 const root = document.documentElement;
 
-darkBtn.addEventListener('click', () => {
-	darkBtn.style.display = 'none';
-	lightBtn.style.display = 'flex';
+const toggleDark = () => {
+	lightBtn.classList.toggle('animate-light');
+	darkBtn.classList.toggle('animate-dark');
 	root.style.setProperty('--clr-navy-900', 'hsl(220, 40%, 13%)');
 	root.style.setProperty('--clr-white', 'hsl(0, 0%, 100%)');
 	root.style.setProperty('--clr-light-200', 'hsl(228, 100%, 98%)');
@@ -13,10 +13,10 @@ darkBtn.addEventListener('click', () => {
 	root.style.setProperty('--clr-light-100', 'hsl(0, 0%, 100%)');
 	root.style.setProperty('--clr-link-text', 'hsl(0, 0%, 100%)');
 	root.style.setProperty('--clr-icon', 'hsl(0, 0%, 100%)');
-});
-lightBtn.addEventListener('click', () => {
-	lightBtn.style.display = 'none';
-	darkBtn.style.display = 'flex';
+};
+const toggleLight = () => {
+	lightBtn.classList.toggle('animate-light');
+	darkBtn.classList.toggle('animate-dark');
 	root.style.setProperty('--clr-navy-900', '#F6F8FF');
 	root.style.setProperty('--clr-white', 'hsl(220, 40%, 13%)');
 	root.style.setProperty('--clr-light-200', 'hsl(217, 35%, 45%)');
@@ -25,4 +25,6 @@ lightBtn.addEventListener('click', () => {
 	root.style.setProperty('--clr-light-100', 'hsl(220, 40%, 13%)');
 	root.style.setProperty('--clr-link-text', 'hsl(217, 35%, 45%)');
 	root.style.setProperty('--clr-icon', 'hsl(217, 35%, 45%)');
-});
+};
+lightBtn.addEventListener('click', toggleLight);
+darkBtn.addEventListener('click', toggleDark);
