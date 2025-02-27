9c7eafe892fe64749d7bfe41661370967e18a94e
Kacper <kacperwach96@gmail.com>
2025-02-27 21:03:47 +0100

    Adjust layout for desktop devices + add hover effects to particular elements

---
+++ src/js/fetchAPI.js
@@ -0,0 +1,7 @@
+const searchImg = document.querySelector('.search-img');
+const searchInput = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
+
+searchImg.addEventListener('click', () => {
+	searchInput.focus();
+});
